2c67f0264904942cd99c981a1b00bd26bd17f76f
Mariee-codes <marion.19745@students.kyu.ac.ke>
2025-06-12 13:44:31 +0300

    learnt variables

--- variables.js
+++ variables.js
@@ -1,77 +1,115 @@
-console.log('Hello World'); 
+// console.log('Hello World'); 
 
-let firstName; 
-firstName ='Steven';
+// let firstName; 
+// firstName ='Steven';
 
-let favouriteFruit='strawberries';
-let favouriteIceCream ='chocolate';
-let favouriteProgrammingLanguage ='JavaScript';
+// let favouriteFruit='strawberries';
+// let favouriteIceCream ='chocolate';
+// let favouriteProgrammingLanguage ='JavaScript';
  
-let lovesCoding = true;
+// let lovesCoding = true;
 
-let favoriteColor;
-console.log(favoriteColor);
+// let favoriteColor;
+// console.log(favoriteColor);
 
-favouriteFruit = null;
+// favouriteFruit = null;
 
-ConstuniqueKey = Symbol();
+// ConstuniqueKey = Symbol();
 
-let course ={
-    name: 'JavaScrit for Beginners',
-    hours: 3
-};
-
-
-
-
-/*objects*/
-letcourse = {
-    name:'Javascript for Beginners' ,
-    hours: 3
-};
-//console.log(course.name);
-//course.name='Javascript Fundamentals';
-//console.log(course.name);
-
-//console.log(course['name']); 
-
-//course['name'] = 'Javascript 101';
-//console.log(course['name']);
-
-let property ='hours';
-
-console.log(course[property]); 
+// let course ={
+//     name: 'JavaScrit for Beginners',
+//     hours: 3
+// };
 
 
 
 
+// /*objects*/
+// letcourse = {
+//     name:'Javascript for Beginners' ,
+//     hours: 3
+// };
+// //console.log(course.name);
+// //course.name='Javascript Fundamentals';
+// //console.log(course.name);
 
-//arrays
-let productColors = ['blue','green'];
-productColors[0] =42;
-console.log(productColors[0]);
-console.log(typeof productColors);
-console.log(productColors.length);
+// //console.log(course['name']); 
 
+// //course['name'] = 'Javascript 101';
+// //console.log(course['name']);
 
+// let property ='hours';
 
-//functions
-function sayHi(name) {
-    console.log('Hi!' +name);
-}
-
-sayHi('steven');
-
-
-/* functons */
-function multiply(num1, num2){
-    return num1 * num2;
-}
-
-console.log(multiply(2, 2) );
+// console.log(course[property]); 
 
 
-/* operators */
-/* -,+,*,/,% */
-let programmingLanguage=javascript
 
+
+
+// //arrays
+// let productColors = ['blue','green'];
+// productColors[0] =42;
+// console.log(productColors[0]);
+// console.log(typeof productColors);
+// console.log(productColors.length);
+
+
+
+// //functions
+// function sayHi(name) {
+//     console.log('Hi!' +name);
+// }
+
+// sayHi('steven');
+
+
+// /* functons */
+// function multiply(num1, num2){
+//     return num1 * num2;
+// }
+
+// console.log(multiply(2, 2) );
+
+
+// /* operators */
+// /* -,+,*,/,% */
+// let programmingLanguage=javascript
+
+// let x;
+// x=123;
+// console.log(x)
+// let age =25;
+// let price =10.99;
+// let gpa =2.1;
+
+// let firstName = 'Bro';
+// let favouriteFood ='pizza';
+
+// console.log(typeof firstName);
+// console.log(`your name is ${firstName}`);
+// console.log(typeof favouriteFood);
+// console.log(`your favouriteFood is ${favouriteFood}`);
+
+// console.log(`you are ${age} years old`);
+// console,log(`The price is $${price}`) ;
+// console.log(`your gpa is : ${gpa}`);
+// console.log(typeof gpa);
+
+// booleans
+// let online =true;
+// let forSale = false;
+// let isStudent = true;
+
+// console.log(`Bro is online: ${online}`);
+// console.log(`Is this car for sale: ${forSale}`);
+// console.log(`Enrolled: ${isStudent}`);
+
+
+
+fullName='Bro code';
+age = 24;
+isStudent = false;
+
+document.getElementById("p1").textContent = `Your name is ${fullName}`;
+document.getElementById("p2").textContent = `You are ${age} years old`;
+document.getElementById("p3").textContent = `Enrolled: ${isStudent}`;
