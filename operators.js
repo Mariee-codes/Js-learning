7032e865e47e7aaa049066b1122734af6c1afb65
Mariee-codes <marion.19745@students.kyu.ac.ke>
2025-06-12 14:20:54 +0300

    learnt operators

--- operators.js
+++ operators.js
@@ -1,2 +1,34 @@
-let n= 5+5*4;
-console.log(n);
+// let n= 5+5*4;
+// console.log(n);
+
+// Arithmetic Operators= operands (values,variables)
+
+
+let students = 30;
+
+// students = students +1;
+// students = students -1;
+// students = students *2;
+// students = students/2;
+// students = students ** 2;
+// // students = students % 2;
+
+
+// students +=1;
+//  students -=1;
+// students *1;
+ //students /=1;
+ // students **=1;
+  //students %=1;
+
+
+// console.log(students);
+// operator precedence
+/*
+1.parenthesis()
+2.exponents
+3.multplication & division &modulo
+4.additon & subtraction
+*/
+// let result = 1+ 2 *3 +4 **2;
+// console.log(result);
