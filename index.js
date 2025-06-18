72813e946f80016051193fa13b346c2f0a2b4c47
Mariee-codes <marion.19745@students.kyu.ac.ke>
2025-06-18 15:38:29 +0300

    type conversion

--- index.js
+++ index.js
@@ -2,14 +2,33 @@
 // 1. EASY WAY = windows prompt
 // 2. PROFESSIONAL WAY = HTML textbox
 
-let username;
-username = window.prompt("whats your username");
-console.log(username);
+// let username;
+// username = window.prompt("whats your username");
+// console.log(username);
 
 
 
- document.getElementById("mySubmit").onclick = function(){
-   username = document.getElementById("myText").value;
-   document.getElementById("myH1").textContent = `Hello ${username}`
-    console.log(username);
- }
+//  document.getElementById("mySubmit").onclick = function(){
+//    username = document.getElementById("myText").value;
+//    document.getElementById("myH1").textContent = `Hello ${username}`
+//     console.log(username);
+//  }
+
+// type conversation  = change the datatype of a value to another (strings,numbers,booleans)
+// let age = window.prompt("how old are you");
+// age = Number(age);
+// age+=1;
+// console.log(age);
+
+let x ="pizza";
+let y ="pizza";
+let z ="pizza";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
+
