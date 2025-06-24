3555c6d471a1fea89e7b54ef47c17d41cd403613
Mariee-codes <marion.19745@students.kyu.ac.ke>
2025-06-24 23:43:47 +0300

    Math objects

--- index.js
+++ index.js
@@ -57,23 +57,52 @@
 // }
 
 // COUNTER PROGRAM
-const decreaseBtn= document.getElementById("decreaseBtn");
-const resetBtn= document.getElementById("resetBtn");
-const increaseBtn= document.getElementById("increaseBtn");
-const countLabel = document.getElementById("countLabel");
-let count = 0;
+// const decreaseBtn= document.getElementById("decreaseBtn");
+// const resetBtn= document.getElementById("resetBtn");
+// const increaseBtn= document.getElementById("increaseBtn");
+// const countLabel = document.getElementById("countLabel");
+// let count = 0;
 
-increaseBtn.onclick = function(){
-    count ++;
-    countLabel.textContent = count;
-}
+// increaseBtn.onclick = function(){
+//     count ++;
+//     countLabel.textContent = count;
+// }
 
-decreaseBtn.onclick = function(){
-    count--;
-    countLabel.textContent= count;
-}
+// decreaseBtn.onclick = function(){
+//     count--;
+//     countLabel.textContent= count;
+// }
 
-resetBtn.onclick = function(){
-    count = 0;
-    countLabel.textContent = count;
-}
+// resetBtn.onclick = function(){
+//     count = 0;
+//     countLabel.textContent = count;
+// }
+
+// Math = built-in object that provides a 
+//       collection of properties and methods
+// Math.PI
+// console.log(Math.PI);
+
+let x = 3;
+let y = 2;
+let z  = 1;
+
+// z= Math.round(x);
+// z = Math.floor(x);
+// z = Math.trunc(x);
+// z = Math.ceil(x);
+// z  = Math.pow(x, y);
+// z = Math.sqrt(x);
+// z = Math.log(x);
+// z= Math.sin(x);
+// z = Math.cos(x);
+// z = Math.tan(x);
+// z = Math.abs(x);
+// z = Math.sign(x);
+
+let max= Math.max(x,y,z);
+let min =Math.min(x,y,z);
+
+
+
+console.log(z);
