b69329a8aeb82bd000af7f43f0bda64073c73cde
Mariee-codes <marion.19745@students.kyu.ac.ke>
2025-07-14 15:27:29 +0300

    switches in js

--- index.js
+++ index.js
@@ -302,24 +302,70 @@
 // let message = age >= 18? "youre an adult" : "youre a minor";
 // console.log(message);
 
-let time = 16;
-let greeting = time < 12? "good morning!" : " good afternoon!";
-console.log(greeting);
+// let time = 16;
+// let greeting = time < 12? "good morning!" : " good afternoon!";
+// console.log(greeting);
 
-let isStudent = true;
-let message = isStudent ? "youre a student" : "youre not a student";
-console.log(message);
+// let isStudent = true;
+// let message = isStudent ? "youre a student" : "youre not a student";
+// console.log(message);
 
-let purchaseAmount = 125;
-let discount = purchaseAmount >=100? 10 :0;
-console.log(`your total is $${purchaseAmount - purchaseAmount * (discount/100)}`);
-
-
+// let purchaseAmount = 125;
+// let discount = purchaseAmount >=100? 10 :0;
+// console.log(`your total is $${purchaseAmount - purchaseAmount * (discount/100)}`);
 
 
 
+// SWITCHES
+let day = 1;
+switch(day){
+    case 1:
+        console.log("its Monday");
+        break;
+    case 2:
+        console.log("its tuesday");
+        break;
+    case 3:
+        console.log("its wednesday");
+        break;
+     case 4:
+        console.log("its thursday");
+        break;
+     case 5:
+        console.log("its friday");
+        break;
+    case 6:
+        console.log("its saturday");
+        break;
+     case 7:
+        console.log("its sunday");
+        break;
+        default:
+            console.log(`${day} its not a day`);       
+}
 
+let testScore = 92;
+let letterGrade;
 
+switch(true){
+    case testScore >= 90:
+        letterGrade = "A" ;
+        break;
+    case testScore >= 80:
+        letterGrade = "B" ;
+        break;
+
+    case testScore >= 70:
+        letterGrade = "C" ;
+        break;
+    case testScore >= 60:
+        letterGrade = "D" ;
+        break;
+        default:
+            letterGrade = "F";
+
+}
+console.log(letterGrade);
 
 
 
