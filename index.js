171c25bb92c0d0a17d46c8bb897b2bde0b461b21
Mariee-codes <marion.19745@students.kyu.ac.ke>
2025-07-14 15:03:09 +0300

    ternary opertaor

--- index.js
+++ index.js
@@ -134,166 +134,126 @@
 
 
 // WEATHER APP
-const weatherForm = document.querySelector(".weatherForm");
-const cityInput = document.querySelector(".cityInput");
-const card = document.querySelector(".card")
-const apiKey = SecretCode();
+// const weatherForm = document.querySelector(".weatherForm");
+// const cityInput = document.querySelector(".cityInput");
+// const card = document.querySelector(".card")
+// const apiKey = SecretCode();
 
-weatherForm.addEventListener("submit", async event => {
+// weatherForm.addEventListener("submit", async event => {
 
-event.preventDefault(); //stops form from reloading page
+// event.preventDefault(); //stops form from reloading page
 
-const city = cityInput.value;
+// const city = cityInput.value;
 
-if(city){
-    try{
-        const weatherData = await getWeatherData(city);
-        getWeatherInfo(weatherData);
-    }
-    catch(error){
-        console.error(error);
-        displayError(error);
-    }
+// if(city){
+//     try{
+//         const weatherData = await getWeatherData(city);
+//         getWeatherInfo(weatherData);
+//     }
+//     catch(error){
+//         console.error(error);
+//         displayError(error);
+//     }
 
     
-}
-else {
-    displayError("please enter a city");
-}
-});
-
-const getWeatherInfo=(data) =>{
-console.log({info:data});
-displayWeatherInfo(data);
-}
-
-
-
-
-async function getWeatherData(city){
-    const apiurl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-const response = await fetch(apiurl);
- console.log(response);
-
-if(!response.ok){
-    throw new Error("Could not fetch weather data");
-}
-return await response.json();
-
-}
-
-
-
-function displayWeatherInfo(data){
-    console.log("displaying")
-    console.log(data);
-    // object destructuring
-    const {name:city, 
-        main:{temp,humidity},
-        weather:[{description,id}]}= data;
-
-        card.textContent="";
-        card.style.display = "flex";
-
-        const cityDisplay = document.createElement("h1");
-        const tempDisplay = document.createElement("p");
-        const humidityDisplay = document.createElement("p");
-        const descDisplay = document.createElement("p");
-        const weatherEmoji = document.createElement("p");
-
-        cityDisplay.textContent = city;
-        tempDisplay.textContent = `${(temp -273).toFixed(1)} \u00B0C`
-        humidityDisplay.textContent = `Humidity: ${humidity}%`;
-        descDisplay.textContent = description;
-        weatherEmoji.textContent = getWeatherEmoji(id);
-
-        cityDisplay.classList.add("cityDisplay");
-        tempDisplay.classList.add("tempDisplay");
-        humidityDisplay.classList.add("humidityDisplay")
-        descDisplay.classList.add("descDisplay");
-        weatherEmoji.classList.add("weatherEmoji")
-
-
-
-        card.appendChild(cityDisplay);
-        card.appendChild(tempDisplay);
-        card.appendChild(humidityDisplay);
-        card.appendChild(descDisplay);
-        card.appendChild(weatherEmoji);
-}
-
-function getWeatherEmoji(weatherId){
-    switch(true){
-        case (weatherId >= 200 && weatherId < 300):
-            return"⛈⚡";
-        case (weatherId >= 300 && weatherId < 400):
-            return"🌧";
-        case (weatherId >= 500 && weatherId < 600):
-            return"🌧";
-        case (weatherId >= 600 && weatherId < 700):
-            return"❄";
-        case (weatherId >= 700 && weatherId < 800):
-            return"🌫";
-        case (weatherId ===800):
-            return"☀";  
-        case (weatherId >= 801 && weatherId < 810):
-            return"☁"; 
-            default:
-                return"?";
-    }
-
-}
-
-function displayError(message){
- errorDisplay = document.createElement("p");
-    errorDisplay.textContent = message;
-    errorDisplay.classList.add("errorDisplay");
-
-    card.textContent = "" 
-    card.style.display = "flex";
-    card.appendChild(errorDisplay);
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const getText=()=>{
-//    const text= "my family loves Marion";
-//     return text;
 // }
-// const promisegetText=async()=>{
-//  const text= "my family loves Marion";
-//     return text;
+// else {
+//     displayError("please enter a city");
 // }
-// const main=async ()=>{
-//     let voidText= getText();
-//     console.log({voidText});
-//     let promisedText= await promisegetText();
-//     console.log({aPromise:promisedText});
+// });
+
+// const getWeatherInfo=(data) =>{
+// console.log({info:data});
+// displayWeatherInfo(data);
 // }
-// main();
+
+
+
+
+// async function getWeatherData(city){
+//     const apiurl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+// const response = await fetch(apiurl);
+//  console.log(response);
+
+// if(!response.ok){
+//     throw new Error("Could not fetch weather data");
+// }
+// return await response.json();
+
+// }
+
+
+
+// function displayWeatherInfo(data){
+//     console.log("displaying")
+//     console.log(data);
+//     // object destructuring
+//     const {name:city, 
+//         main:{temp,humidity},
+//         weather:[{description,id}]}= data;
+
+//         card.textContent="";
+//         card.style.display = "flex";
+
+//         const cityDisplay = document.createElement("h1");
+//         const tempDisplay = document.createElement("p");
+//         const humidityDisplay = document.createElement("p");
+//         const descDisplay = document.createElement("p");
+//         const weatherEmoji = document.createElement("p");
+
+//         cityDisplay.textContent = city;
+//         tempDisplay.textContent = `${(temp -273).toFixed(1)} \u00B0C`
+//         humidityDisplay.textContent = `Humidity: ${humidity}%`;
+//         descDisplay.textContent = description;
+//         weatherEmoji.textContent = getWeatherEmoji(id);
+
+//         cityDisplay.classList.add("cityDisplay");
+//         tempDisplay.classList.add("tempDisplay");
+//         humidityDisplay.classList.add("humidityDisplay")
+//         descDisplay.classList.add("descDisplay");
+//         weatherEmoji.classList.add("weatherEmoji")
+
+
+
+//         card.appendChild(cityDisplay);
+//         card.appendChild(tempDisplay);
+//         card.appendChild(humidityDisplay);
+//         card.appendChild(descDisplay);
+//         card.appendChild(weatherEmoji);
+// }
+
+// function getWeatherEmoji(weatherId){
+//     switch(true){
+//         case (weatherId >= 200 && weatherId < 300):
+//             return"⛈⚡";
+//         case (weatherId >= 300 && weatherId < 400):
+//             return"🌧";
+//         case (weatherId >= 500 && weatherId < 600):
+//             return"🌧";
+//         case (weatherId >= 600 && weatherId < 700):
+//             return"❄";
+//         case (weatherId >= 700 && weatherId < 800):
+//             return"🌫";
+//         case (weatherId ===800):
+//             return"☀";  
+//         case (weatherId >= 801 && weatherId < 810):
+//             return"☁"; 
+//             default:
+//                 return"?";
+//     }
+
+// }
+
+// function displayError(message){
+//  errorDisplay = document.createElement("p");
+//     errorDisplay.textContent = message;
+//     errorDisplay.classList.add("errorDisplay");
+
+//     card.textContent = "" 
+//     card.style.display = "flex";
+//     card.appendChild(errorDisplay);
+
+// }
 
 
 
@@ -301,6 +261,58 @@ function displayError(message){
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // const getText=()=>{
+// //    const text= "my family loves Marion";
+// //     return text;
+// // }
+// // const promisegetText=async()=>{
+// //  const text= "my family loves Marion";
+// //     return text;
+// // }
+// // const main=async ()=>{
+// //     let voidText= getText();
+// //     console.log({voidText});
+// //     let promisedText= await promisegetText();
+// //     console.log({aPromise:promisedText});
+// // }
+// // main();
+
+
+
+// Ternary operators
+// let age = 21;
+// let message = age >= 18? "youre an adult" : "youre a minor";
+// console.log(message);
+
+let time = 16;
+let greeting = time < 12? "good morning!" : " good afternoon!";
+console.log(greeting);
+
+let isStudent = true;
+let message = isStudent ? "youre a student" : "youre not a student";
+console.log(message);
+
+let purchaseAmount = 125;
+let discount = purchaseAmount >=100? 10 :0;
+console.log(`your total is $${purchaseAmount - purchaseAmount * (discount/100)}`);
 
 
 
