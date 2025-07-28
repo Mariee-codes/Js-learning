ddc304a9b9242c147e5a94bbf02a6f087f2196a7
Mariee-codes <marion.19745@students.kyu.ac.ke>
2025-07-28 15:20:03 +0300

    variable scope

--- index.js
+++ index.js
@@ -895,18 +895,30 @@
 
 
 // VARIABLE SCOPE - where a variable is recognized and accesible(local and global)
+// function function1(){
+//     let x = 1;
+//     console.log(x);
+// }
+// function function2(){
+//     // local variable
+//     let x = 2;
+//     console.log(x);
+// }
+// function1();
+// function2();
+
+let x = 3;
+// global variable
+
 function function1(){
-    let x = 1;
-    console.log(x);
-}
-function function2(){
-    // local variable
-    let x = 2;
     console.log(x);
 }
 function1();
-function2();
 
+function function2(){
+    console.log(x);
+}
+function2();
 
 
 
