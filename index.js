9a19e8f832a6650c67f00e5f87857163f083161f
Mariee-codes <marion.19745@students.kyu.ac.ke>
2025-07-28 11:53:26 +0300

    number guessing game

--- index.js
+++ index.js
@@ -770,41 +770,75 @@
 
 
 // FOR LOOP _ Repeat some code for a limited amount of time
-for(let i = 0; i <= 2; i++){
-    console.log("Hello");
-}
+// for(let i = 0; i <= 2; i++){
+//     console.log("Hello");
+// }
 
-for(let i = 0; i <= 2; i++){
-    console.log(i);
-}
+// for(let i = 0; i <= 2; i++){
+//     console.log(i);
+// }
 
-for (let i = 0; i<= 10; i+=2){
-console.log(i);
-}
+// for (let i = 0; i<= 10; i+=2){
+// console.log(i);
+// }
 
-for (let i = 10; i>= 0; i-=2){
-    console.log(i);
-}
-console.log("Happy New Year");
+// for (let i = 10; i>= 0; i-=2){
+//     console.log(i);
+// }
+// console.log("Happy New Year");
 
-for(let i = 1; i<= 20; i++){
-    if(i == 13){
-        continue;
+// for(let i = 1; i<= 20; i++){
+//     if(i == 13){
+//         continue;
+//     }
+//     else{
+//         console.log(i);
+//     }
+// }
+
+// for(let i = 1; i<= 20; i++){
+//     if(i == 13){
+//         break;
+//     }
+//     else{
+//         console.log(i);
+//     }
+// }
+
+// NUMBER GUESSING GAME
+const minNum =1;
+const maxNum = 100;
+const answer = 
+Math.floor(Math.random() * (maxNum - minNum +1)) +minNum;
+console.log(answer);
+
+let attempts = 0;
+let guess;
+let running = true;
+while(running){
+guess = window.prompt(`guess a number between ${minNum} - ${maxNum}`);
+guess = Number(guess);}
+
+if(isNaN(guess)){
+    window.alert("please enter a valid number");
+    
+      if(guess< minNum || guess > maxNum ){
+        window.alert("please enter a valid number");
     }
     else{
-        console.log(i);
-    }
-}
-
-for(let i = 1; i<= 20; i++){
-    if(i == 13){
-        break;
-    }
-    else{
-        console.log(i);
-    }
-}
-
+        attempts++;
+        if(guess < answer){
+            window.alert("too low");
+        }
+        else if(guess > answer){
+            window.alert("too high");
+        }
+        else{
+            window.alert(`CORRECT! The answer was ${answer} . It took you ${attempts} attempts`);
+            running = false;
+        }
+    }}
+// console.log(typeof guess, guess);
 
 
 
