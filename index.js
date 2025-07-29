d6df57cbb665d3fcfd33781ee8ff994acafee75a
Mariee-codes <marion.19745@students.kyu.ac.ke>
2025-07-29 15:16:07 +0300

    js arrays

--- index.js
+++ index.js
@@ -921,29 +921,41 @@
 // function2();
 
 // TEMPERATURE CONVERSION PROGRAM
-const textBox = document.getElementById("textBox");
-const toFahrenheit = document.getElementById("toFahrenheit");
-const toCelcius = document.getElementById("toCelcius");
-const result = document.getElementById("result");
-let temp;
+// const textBox = document.getElementById("textBox");
+// const toFahrenheit = document.getElementById("toFahrenheit");
+// const toCelcius = document.getElementById("toCelcius");
+// const result = document.getElementById("result");
+// let temp;
 
-function convert(){
-    if(toFahrenheit.checked){
-        temp =Number(textBox.value);
-        temp = temp * 9/5 + 32;
-        result.textContent = temp.toFixed(1) + "°F";
-    }
-    else if(toCelcius.checked){
-         temp = Number(textBox.value);
-         temp = (temp - 32) * 5/9;
-         result.textContent = temp.toFixed() + "°C";
-    }
-    else{
-        result.textContent = "Select a unit";
-    }
+// function convert(){
+//     if(toFahrenheit.checked){
+//         temp =Number(textBox.value);
+//         temp = temp * 9/5 + 32;
+//         result.textContent = temp.toFixed(1) + "°F";
+//     }
+//     else if(toCelcius.checked){
+//          temp = Number(textBox.value);
+//          temp = (temp - 32) * 5/9;
+//          result.textContent = temp.toFixed() + "°C";
+//     }
+//     else{
+//         result.textContent = "Select a unit";
+//     }
 
-}
+// }
 
+// JAVASCRIPT ARRAYS = a variable structure that can hold more than 1 value
+let fruits = ["apple", "orange", "banana", "pear"];
+
+fruits[1] = "coconut";
+fruits.push("coconut");
+// add to the end of the array
+
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+console.log(fruits[3]);
+console.log(fruits[4]);
 
 
 
