a0f9afd4966202be322ea913b71a38374654f8f6
Mariee-codes <marion.19745@students.kyu.ac.ke>
2025-07-30 15:31:46 +0300

    rest parameters

--- index.js
+++ index.js
@@ -1048,30 +1048,50 @@
 
 
 // jAVASCRIPT REST PARAMETERS
-function openFridge(...foods){
-    console.log(...foods);
+// function openFridge(...foods){
+//     console.log(...foods);
+// }
+// function getFood(...foods){
+//     return foods;
+// }
+
+//     const food1 = "pizza";
+//     const food2 = "hamburger";
+//     const food3 = "hotdog";
+//     const food4 = "sushi";
+//     const food5 = "ramen";
+
+// //openFridge(food1,food2,food3,food4,food5);
+// const foods = getFood(food1,food2,food3,food4,food5);
+// console.log(foods);
+
+function sum(...numbers){
+    let result = 0;
+    for(let number of numbers){
+        result += number;
+    }
+    return result;
 }
-function getFood(...foods){
-    return foods;
+const total = sum(1,2,3,4);
+console.log(`your total is $${total}`);
+
+function getAverage(...numbers){
+    let result = 0;
+    for(let number of numbers){
+        result += number;
+    }
+    return result / numbers.length;
 }
-
-    const food1 = "pizza";
-    const food2 = "hamburger";
-    const food3 = "hotdog";
-    const food4 = "sushi";
-    const food5 = "ramen";
-
-//openFridge(food1,food2,food3,food4,food5);
-const foods = getFood(food1,food2,food3,food4,food5);
-console.log(foods);
+const total1 = getAverage(100,20,37,49,58);
+console.log(total);
 
 
 
-
-
-
-
-
+function combineStrings(...strings){
+    return strings.join(" ");
+}
+const fullName = combineStrings("Mr.","John","Doe","111");
+console.log(fullName);
 
 
 
