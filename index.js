dd3477c18e754a799606dd24980ab2409a0bb596
Mariee-codes <marion.19745@students.kyu.ac.ke>
2025-07-31 16:24:51 +0300

    js dice roller program

--- index.js
+++ index.js
@@ -1095,6 +1095,24 @@
 
 
 // JAVASCRIPT DICE ROLLER PROGRAM
+function rollDice(){
+    const numOfDice = document.getElementById("numOfDice").value;
+    const diceResult = document.getElementById("diceResult");
+    const diceImages = document.getElementById("diceImages");
+    const values=[];
+    const images =[];
+
+    for(let i =0; i< numOfDice; i++){
+        const value = Math.floor(Math.random() * 6) +1;
+        //console.log(value);
+        values.push(value);
+        images.push(`<img src="dice_images/${value}.png"alt="Dice ${value}">`);
+
+    }
+   // console.log(values);
+   diceResult.textContent = `dice: ${values.join(', ')}`;
+   diceImages.innerHTML= images.join('');
+}
 
 
 
