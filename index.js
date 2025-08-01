982cdc62ca8877419291cadedcb40ec7359f3aab
Mariee-codes <marion.19745@students.kyu.ac.ke>
2025-08-01 17:35:57 +0300

    callbacks

--- index.js
+++ index.js
@@ -1189,6 +1189,20 @@ function goodbye(){
 
 
 
+sum(displayPage, 1,2);
+function sum(callback, x, y){
+    let result = x + y;
+    callback(result);
+}
+function displayConsole(result){
+    console.log(result);
+}
+
+function displayPage(result){
+    document.getElementById("myH1").textContent = result;
+}
+
+
 
 
 
