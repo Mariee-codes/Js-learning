7e1a2abbc18142fb6d4535a2a768e5b415d21a9c
Mariee-codes <marion.19745@students.kyu.ac.ke>
2025-08-01 15:34:19 +0300

    random password generator

--- index.js
+++ index.js
@@ -1095,25 +1095,72 @@
 
 
 // JAVASCRIPT DICE ROLLER PROGRAM
-function rollDice(){
-    const numOfDice = document.getElementById("numOfDice").value;
-    const diceResult = document.getElementById("diceResult");
-    const diceImages = document.getElementById("diceImages");
-    const values=[];
-    const images =[];
+// function rollDice(){
+//     const numOfDice = document.getElementById("numOfDice").value;
+//     const diceResult = document.getElementById("diceResult");
+//     const diceImages = document.getElementById("diceImages");
+//     const values=[];
+//     const images =[];
 
-    for(let i =0; i< numOfDice; i++){
-        const value = Math.floor(Math.random() * 6) +1;
-        //console.log(value);
-        values.push(value);
-        images.push(`<img src="dice_images/${value}.png"alt="Dice ${value}">`);
+//     for(let i =0; i< numOfDice; i++){
+//         const value = Math.floor(Math.random() * 6) +1;
+//         //console.log(value);
+//         values.push(value);
+//         images.push(`<img src="dice_images/${value}.png"alt="Dice ${value}">`);
 
+//     }
+//    // console.log(values);
+//    diceResult.textContent = `dice: ${values.join(', ')}`;
+//    diceImages.innerHTML= images.join('');
+// }
+
+//RANDOM PASSWORD GENERATOR
+function generatePassword(length,includeLowercase,includeUppercase,includeNumbers,includeSymbols){
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numberChars = "0123456789"
+    const symbolChars ="!@#$%^&*()-+";
+
+    let allowedChars= " ";
+    let password = " ";
+
+    allowedChars += includeLowercase? lowercaseChars : " ";
+    allowedChars += includeUppercase? uppercaseChars : " ";
+    allowedChars += includeNumbers ?  numberChars : " ";
+    allowedChars += includeSymbols ? symbolChars : " ";
+
+
+    if(length<= 0){
+        return `(password length must be at least 1)`;
     }
-   // console.log(values);
-   diceResult.textContent = `dice: ${values.join(', ')}`;
-   diceImages.innerHTML= images.join('');
+    if(allowedChars.length === 0){
+        return `(At least 1 set of character needs to be selected)`;
+    }
+
+    for(let i=0; i<length; i++){
+        const randomIndex = Math.floor(Math.random() * allowedChars.length);
+        password += allowedChars[randomIndex];
+    }
+
+
+   // console.log(allowedChars);
+return password;
 }
 
+
+const passwordLength = 12;
+const includeLowercase = true;
+const includeUppercase = true;
+const includeNumbers = true;
+const includeSymbols = true;
+
+const password =generatePassword(passwordLength,
+                                 includeLowercase,
+                                includeUppercase,
+                                includeNumbers,
+                                includeSymbols);
+                            
+console.log(`Generated Password:${password}`);
 
 
 
