afbcc3e69221e797fc527fe2644695963822d11a
Mariee-codes <marion.19745@students.kyu.ac.ke>
2025-08-04 18:44:34 +0300

    forEach method

--- index.js
+++ index.js
@@ -1203,34 +1203,48 @@
 // }
 
 // FOR EACH METHOD
-let numbers = [1,2,3,4,5];
+// let numbers = [1,2,3,4,5];
 
-numbers.forEach(double);
-numbers.forEach(display);
-numbers.forEach(tripple);
-numbers.forEach(square);
-numbers.forEach(cube);
+// numbers.forEach(double);
+// numbers.forEach(display);
+// numbers.forEach(tripple);
+// numbers.forEach(square);
+// numbers.forEach(cube);
 
 
-function double(element,index,array){
-    array[index] = element *2;
-}
+// function double(element,index,array){
+//     array[index] = element *2;
+// }
 
-function tripple(element,index,array){
-    array[index] = element *3;
+// function tripple(element,index,array){
+//     array[index] = element *3;
     
+// }
+// function display(element){
+//     console.log(element);
+// }
+// function square(element,index,array){
+// array[index] = Math.pow(element, 2);
+// }
+// function cube(element){
+//     array[index]= Math.pow(element,3);
+// }
+
+let fruits = ["apple","orange","banana","coconut"];
+
+fruits.forEach(upperCase);
+fruits.forEach(dispaly);
+fruits.forEach(capitalize);
+
+function upperCase(element,index,array){
+    array[index]= element.toUpperCase();
 }
-function display(element){
+function dispaly(element){
     console.log(element);
 }
-function square(element,index,array){
-array[index] = Math.pow(element, 2);
+function capitalize(element){
+    array[index] = element.charAt(0).toUpperCase() + element.slice(1);
 }
-function cube(element){
-    array[index]= Math.pow(element,3);
-}
-
-
 
 
 
