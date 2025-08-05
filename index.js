36e40a9de8dffc6057a73678f6fb697911217fcd
Mariee-codes <marion.19745@students.kyu.ac.ke>
2025-08-05 13:50:21 +0300

    map method

--- index.js
+++ index.js
@@ -1230,23 +1230,37 @@
 //     array[index]= Math.pow(element,3);
 // }
 
-let fruits = ["apple","orange","banana","coconut"];
+// let fruits = ["apple","orange","banana","coconut"];
 
-fruits.forEach(upperCase);
-fruits.forEach(dispaly);
-fruits.forEach(capitalize);
+// fruits.forEach(upperCase);
+// fruits.forEach(dispaly);
+// fruits.forEach(capitalize);
 
-function upperCase(element,index,array){
-    array[index]= element.toUpperCase();
+// function upperCase(element,index,array){
+//     array[index]= element.toUpperCase();
+// }
+// function dispaly(element){
+//     console.log(element);
+// }
+// function capitalize(element){
+//     array[index] = element.charAt(0).toUpperCase() + element.slice(1);
+// }
+
+// JS MAPPING METHODS
+const numbers = [1,2,3,4,5];
+const squares = numbers.map(square);
+const cubes = numbers.map(cubes);
+
+console.log(squares);
+console.log(cube);
+
+function square(element){
+    return Math.pow(element,2)
 }
-function dispaly(element){
-    console.log(element);
-}
-function capitalize(element){
-    array[index] = element.charAt(0).toUpperCase() + element.slice(1);
-}
 
-
+function cube(element){
+   return Math.pow(element, 3)
+}
 
 
 
