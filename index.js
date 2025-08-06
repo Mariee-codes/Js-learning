191c6f8f7a2ab33c32a6e2bbcb2720f460a0eea5
Mariee-codes <marion.19745@students.kyu.ac.ke>
2025-08-06 14:50:15 +0300

    function expressions

--- index.js
+++ index.js
@@ -1336,33 +1336,37 @@
 
 // REDUCE METHOD
 // .reduce() = reduce elements of an array to a single value
-const prices = [5, 30, 10, 25, 15, 20];
+//const prices = [5, 30, 10, 25, 15, 20];
 
-const total = prices.reduce(sum);
+// const total = prices.reduce(sum);
 
-console.log(`$${total.toFixed(2)}`);
-// previous + next
-function sum(accumulator, element){
-    return accumulator + element;
+// console.log(`$${total.toFixed(2)}`);
+// // previous + next
+// function sum(accumulator, element){
+//     return accumulator + element;
+// }
+
+// const grades = [75, 50, 90, 80, 65,95];
+// const maximum = grades.reduce(getMax);
+// const minimum = grades.reduce(getMin);
+
+// console.log(maximum);
+// console.log(minimum);
+
+// function getMax(accumulator, element){
+//     return Math.max(accumulator,element);
+// }
+
+// function getMin(accumulator, element){
+//     return Math.min(accumulator, element);
+// }
+
+
+//JS FUNCTION EXPRESSIONS
+const hello = function(){
+    console.log("hello");
 }
-
-const grades = [75, 50, 90, 80, 65,95];
-const maximum = grades.reduce(getMax);
-const minimum = grades.reduce(getMin);
-
-console.log(maximum);
-console.log(minimum);
-
-function getMax(accumulator, element){
-    return Math.max(accumulator,element);
-}
-
-function getMin(accumulator, element){
-    return Math.min(accumulator, element);
-}
-
-
-
+hello();
 
 
 
