73b86560160c787a5a1cf52bfc0548e6ae98c5a6
Mariee-codes <marion.19745@students.kyu.ac.ke>
2025-08-06 17:03:51 +0300

    objects in js

--- index.js
+++ index.js
@@ -1435,7 +1435,18 @@ setTimeout(function(){
 
 setTimeout(() => console.log("hello"), 3000);
 
+const numbers = [1,2,3,4,5,6];
 
+const squares = numbers.map((element) => Math.pow(element,2));
+const cubes = numbers.map((element) => Math.pow(element,3));
+const evenNums = numbers.filter((element)=> element % 2=== 0);
+const oddNums = numbers.filter((element)=> element % 2 !== 0);
+const total = numbers.reduce((accumulator,element)=> accumulator + element );
+
+console.log(cubes);
+console.log(squares);
+console.log(evenNums);
+console.log(oddNums);
 
 
 
