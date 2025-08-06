93d897a367bdc2f0a880cd85c78582d0044dd764
Mariee-codes <marion.19745@students.kyu.ac.ke>
2025-08-06 15:26:18 +0300

    function expressions

--- index.js
+++ index.js
@@ -1363,12 +1363,23 @@
 
 
 //JS FUNCTION EXPRESSIONS
-const hello = function(){
-    console.log("hello");
+setTimeout(function(){
+    console.log("Hello");
+}, 3000);
+
+const numbers = [1,2,3,4,5,6];
+const squares = numbers.map(square);
+
+console.log(squares);
+
+function square(element){
+    return Math.pow(element,2);
 }
-hello();
+//function declaration
 
+//function expression
 
+//const squares = numbers.map(square);
 
 
 
