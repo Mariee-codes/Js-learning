59d0cd3122b8c8a5f3f29cec4adb7bf29e8e0142
Mariee-codes <marion.19745@students.kyu.ac.ke>
2025-08-12 15:25:22 +0300

    constructors

--- index.js
+++ index.js
@@ -1526,6 +1526,34 @@ console.log(car2.model);
 console.log(car2.year);
 console.log(car2.color);
 
+function Car(a,b,c,d){
+    this.make = a,
+    this.model = b,
+    this.year = c,
+    this.color = d
+}
+const car1 = new Car("Ford","Mustang",2024,"red");
+const car2 = new Car ("chevrolet","camaro",2025,"blue");
+
+console.log(car1.make);
+console.log(car1.model);
+console.log(car1.year);
+console.log(car1.color);
+
+console.log(car2.make);
+console.log(car2.model);
+console.log(car2.year);
+console.log(car2.color);
+
+
+
+
+
+
+
+
+
+
 
 
 
