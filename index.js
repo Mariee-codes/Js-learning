f12304771eee0bacae5d4dfe4af0923615bbc32e
Mariee-codes <marion.19745@students.kyu.ac.ke>
2025-08-13 16:28:46 +0300

    static keyword

--- index.js
+++ index.js
@@ -1553,36 +1553,56 @@
 
 
 // CLASSES
-class Product{
-    constructor(name, price){
-        this.name = name;
-        this.price = price;
+// class Product{
+//     constructor(name, price){
+//         this.name = name;
+//         this.price = price;
+//     }
+
+//     displayProduct(){
+//         console.log(`product: ${this.name}`);
+//         console.log(`price: $${this.price.toFixed(2)}`);
+//     }
+
+//     calculateTotal(salesTax){
+//         return this.price + (this.price * salesTax);
+//     }
+// }
+
+// const salesTax = 0.05;
+
+// const product1 = new Product("shirt", 19.99);
+// const product2 = new Product("pants", 22.99);
+// const product3 = new Product("underwear", 100.99);
+
+// product1.displayProduct();
+// product2.displayProduct();
+// product3.displayProduct();
+
+// const total = product1.calculateTotal( salesTax);
+// console.log(`Total price(with tax): $${total.toFixed(2)}`);
+
+
+// STATIC KEYWORD
+class MathUtil{
+    static PI = 3.14159;
+
+    static getDiameter(radius){
+        return radius = 2;
+    }
+    static getCircumference(radius){
+        return 2* this.PI * radius;
+    }
+    static getArea(radius){
+        return this.PI * radius * radius;
+    }
     }
 
-    displayProduct(){
-        console.log(`product: ${this.name}`);
-        console.log(`price: $${this.price.toFixed(2)}`);
-    }
 
-    calculateTotal(salesTax){
-        return this.price + (this.price * salesTax);
-    }
-}
-
-const salesTax = 0.05;
-
-const product1 = new Product("shirt", 19.99);
-const product2 = new Product("pants", 22.99);
-const product3 = new Product("underwear", 100.99);
-
-product1.displayProduct();
-product2.displayProduct();
-product3.displayProduct();
-
-const total = product1.calculateTotal( salesTax);
-console.log(`Total price(with tax): $${total.toFixed(2)}`);
-
-
+console.log(MathUtil.PI);
+console.log(MathUtil.getDiameter(10));
+console.log(MathUtil.getCircumference(10));
+console.log(MathUtil.getArea(10));
 
 
 
