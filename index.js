0aabcdbd9bf436f769b9d318d644c5b926a2dc7f
Mariee-codes <marion.19745@students.kyu.ac.ke>
2025-08-13 14:43:47 +0300

    constructors

--- index.js
+++ index.js
@@ -1511,7 +1511,8 @@ function Car(make,model,year,color){
     this.make = make,
     this.model = model,
     this.year = year,
-    this.color = color
+    this.color = color,
+    this.drive = function(){console.log(`you drive the ${this.model}`)}
 }
 const car1 = new Car("Ford","Mustang",2024,"red");
 const car2 = new Car ("chevrolet","camaro",2025,"blue");
@@ -1520,31 +1521,35 @@ console.log(car1.make);
 console.log(car1.model);
 console.log(car1.year);
 console.log(car1.color);
+console.log(car1.drive());
 
 console.log(car2.make);
 console.log(car2.model);
 console.log(car2.year);
 console.log(car2.color);
 
-function Car(a,b,c,d){
-    this.make = a,
-    this.model = b,
-    this.year = c,
-    this.color = d
-}
-const car1 = new Car("Ford","Mustang",2024,"red");
-const car2 = new Car ("chevrolet","camaro",2025,"blue");
+// function Car(a,b,c,d){
+//     this.make = a,
+//     this.model = b,
+//     this.year = c,
+//     this.color = d
+// }
+// const car1 = new Car("Ford","Mustang",2024,"red");
+// const car2 = new Car ("chevrolet","camaro",2025,"blue");
 
-console.log(car1.make);
-console.log(car1.model);
-console.log(car1.year);
-console.log(car1.color);
+// console.log(car1.make);
+// console.log(car1.model);
+// console.log(car1.year);
+// console.log(car1.color);
 
-console.log(car2.make);
-console.log(car2.model);
-console.log(car2.year);
-console.log(car2.color);
+// console.log(car2.make);
+// console.log(car2.model);
+// console.log(car2.year);
+// console.log(car2.color);
 
+car1.drive();
+car2.drive();
+car3.drive();
 
 
 
