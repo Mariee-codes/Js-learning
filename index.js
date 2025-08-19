53d3af411cbe273b7b1e74d45b1507bc43ad9997
Mariee-codes <marion.19745@students.kyu.ac.ke>
2025-08-19 11:32:54 +0300

    destructuring

--- index.js
+++ index.js
@@ -2119,21 +2119,45 @@
 // EXAMPLE 1
 // SWAP THE VALUE OF TWO VARIABLES
 
-let a = 1;
-let b = 2;
-[a,b] = [b,a];
+// let a = 1;
+// let b = 2;
+// [a,b] = [b,a];
 
-console.log(a);
-console.log(b);
+// console.log(a);
+// console.log(b);
 
 const colors = ["red","green","blue","black","white"];
-[colors[0], colors[4]] = [colors[4], colors[0]];
-console.log(colors);
+// [colors[0], colors[4]] = [colors[4], colors[0]];
+// console.log(colors);
+
+const [firstColor,secondColor,thirdColor,... extraColors] = colors;
+
+console.log(firstColor);
+console.log(secondColor);
+console.log(thirdColor);
+console.log(extraColors);
 
 
+// EXTRACT VALUES FROM OBJECTS
+const person1 = {
+    firstName: "spongebob",
+    lastName: "squarepants",
+    age:30,
+    job:"fry cook"
+}
 
+const person2 = {
+    firstName: "patrick",
+    lastName: "star",
+    age:40,
+}
 
+const {firstName, lastName,age,job="unemployed"} = person2;
 
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(job);
 
 
 
