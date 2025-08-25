cf82e1bb5795df19aeee60fc4f107fb0e23f14f2
Mariee-codes <marion.19745@students.kyu.ac.ke>
2025-08-26 01:34:21 +0300

    shuffle an array

--- index.js
+++ index.js
@@ -2266,6 +2266,19 @@ console.log(people);
 people.sort((a,b) => a.name.localeCompare(b.name));
 console.log(people);
 
+// shuffle an array
+// fisher-Yates algorithm
+const cards = ['A',2,3,4,5,6,7,8,9,10,'J','Q','K'];
+shuffle(cards);
+console.log(cards);
+
+function shuffle(array){
+    for(let i = array.length - 1; i>0; i--){
+        const random = Math.floor(Math.random() *(i +1));
+        [array[i], array[random]] - [array[random], array[i]];
+            
+        }
+    }
 
 
 
