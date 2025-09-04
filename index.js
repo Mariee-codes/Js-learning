55f364fa0ce2e200361eafa706c0ce3b795c099d
Mariee-codes <marion.19745@students.kyu.ac.ke>
2025-09-04 13:04:07 +0300

    js setTimeout

--- index.js
+++ index.js
@@ -2354,35 +2354,41 @@
 // counter.increment();
 
 // console.log(`The current count is ${counter.getCount()}`);
-function createGame(){
-let score = 0;
+// function createGame(){
+// let score = 0;
 
-function increaseScore(points){
-    score += points;
-    console.log(`+${points}pts`);
+// function increaseScore(points){
+//     score += points;
+//     console.log(`+${points}pts`);
+// }
+
+// function decreaseScore(points){
+//     score -= points;
+//     console.log(`-${points}pts`);
+// }
+// function getScore(){
+//     return score;
+// }
+// return{increaseScore, decreaseScore,getScore};
+
+// }
+
+// const game = createGame();
+
+// game.increaseScore(5);
+// game.decreaseScore(9);
+// console.log(`the final score is ${getScore()}pts`)
+
+
+// JAVASCRIPT SET TIMEOUT FUNCTION
+function sayHello(){
+    window.alert("hello");
 }
 
-function decreaseScore(points){
-    score -= points;
-    console.log(`-${points}pts`);
-}
-function getScore(){
-    return score;
-}
-return{increaseScore, decreaseScore,getScore};
+setTimeout(sayHello, 3000);
 
-}
-
-const game = createGame();
-
-game.increaseScore(5);
-game.decreaseScore(9);
-console.log(`the final score is ${getScore()}pts`)
-
-
-
-
-
+setTimeout(function(){window.alert("hello")}, 3000);
+setTimeout(()=> window.alert("hello"),3000);
 
 
 
